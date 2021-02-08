4c92cccd7e17fcbe55f5d10f33cf94a15324e741
Mille Jørgsholm Bierrings <mill1197@stud.kea.dk>
2021-02-08 22:45:48 +0100

    adding commands and making the assignment

--- capitalization3.js
+++ capitalization3.js
@@ -1,8 +1,16 @@
 "use strict";
 
-const firstname = "Annemette";
-const first = firstname.substring(2, 3).toUpperCase();
-const second = firstname.substring(0, 1).toLowerCase();
-const third = firstname.substring(3, 9).toLowerCase();
+const firstname = "Peter";
 
-console.log(`${second}${first}${third}`);
+//Declaring 3 variables "first, second, and third" because the string has to be devided in lowercase, uppercase and lowercase
+
+//Characters at position 0 to 2 has to be written in lowercase
+const first = firstname.substring(0, 2).toLowerCase();
+
+//Characters at position 2 to 3 has to be written in uppercase
+const second = firstname.substring(2, 3).toUpperCase();
+
+//Characters at position 3 to 25has to be written in lowercase
+const third = firstname.substring(3, 5).toLowerCase();
+
+console.log(`${first}${second}${third}`);
