840ed4514a2165e4f0fca23f435b1154d3b4812b
Mille Jørgsholm Bierrings <mill1197@stud.kea.dk>
2021-02-08 22:49:39 +0100

    making the assignment in the right way and adding commands

--- capitalization.js
+++ capitalization.js
@@ -1,7 +1,13 @@
 "use strict";
 
-const firstname = "annemette";
-const first = firstname.substring(0,1).toUpperCase();
-const second = firstname.substring(1,9).toLowerCase();
+const firstname = "Peter";
 
-console.log (`${first}${second}`);
+//Declaring 2 variables "first and second" because the string has to be devided in uppercase and lowercase
+
+//Characters at position 0 to 1 (the first letter) has to be written in uppercase
+const first = firstname.substring(0, 1).toUpperCase();
+
+//Characters at position 1 to 5 (the rest of the letters) has to be written in lowercase
+const second = firstname.substring(1, 5).toLowerCase();
+
+console.log(`${first}${second}`);
