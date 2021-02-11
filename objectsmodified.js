b63ac4a89cf773c68c000781c2ad945261bcffd0
Mille Jørgsholm Bierrings <mill1197@stud.kea.dk>
2021-02-11 13:49:12 +0100

    age is modified

---
+++ objectsmodified.js
@@ -0,0 +1,13 @@
+"use strict";
+
+//Using the object from objectbasics.js and changing the variable from let to const
+
+const person1 = {
+  firstName: "Peter",
+  age: 29,
+  student: false,
+};
+
+//Changing the name from 29 to 30
+person1.age++;
+console.log(person1);
