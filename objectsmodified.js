ce9d97abae45a16cb7b9badcaa61d3da3b14635b
Mille Jørgsholm Bierrings <mill1197@stud.kea.dk>
2021-02-11 13:58:05 +0100

    const vs let

--- objectsmodified.js
+++ objectsmodified.js
@@ -11,3 +11,14 @@ const person1 = {
 //Changing the name from 29 to 30
 person1.age++;
 console.log(person1);
+
+//Creating another object
+const person2 = {
+  firstName: "Mille",
+  age: 21,
+  student: true,
+};
+
+//The variable itself can't be reassignted if it's a const
+//The objects can't be changed but the properties can
+person1 = person2;
