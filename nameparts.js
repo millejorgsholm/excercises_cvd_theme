0bdb2be363d15f3e77fef462a778c686163b3c4b
Mille Jørgsholm Bierrings <mill1197@stud.kea.dk>
2021-02-15 10:26:14 +0100

    parameters excercise added

--- nameparts.js
+++ nameparts.js
@@ -1,22 +1,26 @@
 "use strict";
 
-const fullName = "Peter Heronimous Lind";
+function getNameParts(fullName) {
+  fullName = "Peter Heronimous Lind";
 
-//Grunden til jeg erklærer en first- og lastSpace varibel er for at kunne bruge mellemrummene til at opdele min "main string"
-//Erklærer en konstant variabel, som "tager fat i" det første ord før det første mellemrum. Opdele blandt mellemrummene
-const firstSpace = fullName.indexOf(" ");
-//Laver en substring der sørger for at jeg kan bruge det andet mellemrum som en seperator i min "main string"
-const lastSpace = fullName.lastIndexOf(" ");
+  //Grunden til jeg erklærer en first- og lastSpace varibel er for at kunne bruge mellemrummene til at opdele min "main string"
+  //Erklærer en konstant variabel, som "tager fat i" det første ord før det første mellemrum. Opdele blandt mellemrummene
+  const firstSpace = fullName.indexOf(" ");
+  //Laver en substring der sørger for at jeg kan bruge det andet mellemrum som en seperator i min "main string"
+  const lastSpace = fullName.lastIndexOf(" ");
 
-//Laver en substring, der tager fat i "fullName" på position 0 før første mellemrum, hvilket er navnet "Peter"
-const firstName = fullName.substring(0, firstSpace);
+  //Laver en substring, der tager fat i "fullName" på position 0 før første mellemrum, hvilket er navnet "Peter"
+  const firstName = fullName.substring(0, firstSpace);
 
-//Laver en substring af fullName, som tager fat i "Heronimous", som er mellem firstSpace ogg lastSpace
-const middleName = fullName.substring(firstSpace, lastSpace).trim();
+  //Laver en substring af fullName, som tager fat i "Heronimous", som er mellem firstSpace ogg lastSpace
+  const middleName = fullName.substring(firstSpace, lastSpace).trim();
 
-//Laver en substring af fullName, som tager fat i "Lind", som er navnet efter det sidste mellemrum
-const lastName = fullName.substring(lastSpace);
+  //Laver en substring af fullName, som tager fat i "Lind", som er navnet efter det sidste mellemrum
+  const lastName = fullName.substring(lastSpace);
 
-console.log(`first is ${firstName}
+  console.log(`first is ${firstName}
 middle is ${middleName}
 last is ${lastName}`);
+}
+
+getNameParts();
