568aed147f72e5cfed82bc30c9e863c9f0b28aac
Mille Jørgsholm Bierrings <mill1197@stud.kea.dk>
2021-02-22 11:06:47 +0100

    array sorting excercise

---
+++ sorting.js
@@ -0,0 +1,10 @@
+//Creating people-array
+const people = ["Harry", "Ron", "Hermione", "Neville"];
+
+//It sorts the array alphabetically
+people.sort();
+
+//First we sorted it alphabetically, here we reverse it so that Ron will be the first name to appear
+people.reverse();
+
+console.log(people);
